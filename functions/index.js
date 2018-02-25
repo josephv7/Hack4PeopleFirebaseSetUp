ecf87e1746d5311d5943772a49804236c819a1ff
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-02-25 10:25:35 +0530

    cloud function for answering audio added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
+exports.onAnswer = functions.firestore.document('AudioFiles/{fileId}')
+  .onWrite(event=>{
+    const document = event.data.exists ? event.data.data() : null;
+    if(document.answer.trim()!=="") {
+      return event.data.ref.set({status: true}, {merge: true});
+    }
+  })
