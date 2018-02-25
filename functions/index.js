3c2606bcac8acd970cc90b0c02116accc6245e41
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-02-25 11:55:11 +0530

    final..before presenetation

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,25 @@ exports.onAnswer = functions.firestore.document('AudioFiles/{fileId}')
       return event.data.ref.set({status: true}, {merge: true});
     }
   })
+
+
+
+
+exports.onAnswer = functions.firestore.document('Done/{fileId}')
+  .onCreate(event=>{
+    const document = event.data.exists ? event.data.data() : null;
+    const farmerId = document.farmerId;
+    const amount = parseFloat(document.amount);
+    const farmerRef = firestore.doc(`farmers/${farmerId}`);
+    if(!farmerRef) return null;
+    return farmerRef.get().then((doc)=>{
+      if (doc.exists) {
+        const alreadyAmount = doc.data().amount;
+        const newAmount = parseFloat(alreadyAmount)+amount;
+        return farmerRef.set({amount: newAmount}, {merge: true});
+      }
+      return null;
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+  })
